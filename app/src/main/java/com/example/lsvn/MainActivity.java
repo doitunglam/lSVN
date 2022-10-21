24d808be1874cad5524c6ba5f1522a686369a5f4
DoiTungLam <doitunglam1@gmail.com>
2022-10-21 15:44:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lsvn/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lsvn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
